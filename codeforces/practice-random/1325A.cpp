cf23a5941fff5c2f332481d8465cf19ec5106a78
Vid Furlan <nalrufdiv@gmail.com>
2024-05-23 21:47:51 +0200

    Codeforces 1325A 1370A

---
+++ codeforces/practice-random/1325A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <numeric>
+using namespace std;
+
+using ll = long long;
+using pll = pair<long, long>;
+using pii = pair<int, int>;
+
+const ll MOD = 1e9 + 7;
+const ll LL_INF = 1e18;
+
+void solve() {
+    int n; cin >> n;
+    cout << 1 << " " << n - 1 << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
+
+    int t; cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
---
+++ codeforces/practice-random/1370A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <climits>
+#include <iostream>
+#include <numeric>
+#include <vector>
+using namespace std;
+
+using ll = long long;
+using pll = pair<long, long>;
+using pii = pair<int, int>;
+
+const ll MOD = 1e9 + 7;
+const ll LL_INF = 1e18;
+
+void solve() {
+    int n; cin >> n;
+    cout << n / 2 << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
+
+    int t; cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
