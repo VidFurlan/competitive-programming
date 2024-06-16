51725ece1e39aacc0e4a9c9c712595b26b5089e2
Vid Furlan <nalrufdiv@gmail.com>
2024-06-16 22:20:38 +0200

    Codeforces 1978A

---
+++ codeforces/practice-random/1978A.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+#define ll long long
+#define pll pair<ll, ll>
+#define pii pair<int, int>
+
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define FORR2(x,y,arr) for(auto& [x,y]:arr)
+#define ALL(a) (a.begin()),(a.end())
+
+const ll MOD = 1e9 + 7;
+
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n);
+    FORR(i, a) cin >> i;
+    int ans = 0;
+    for (int i = 0; i < n - 1; i++)
+        ans = max(ans, a[i] + a.back());
+    cout << ans << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
+
+    int t; cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
