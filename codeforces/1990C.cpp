7dd988bda50654ef2d26752957530a163407292c
Vid Furlan <nalrufdiv@gmail.com>
2024-08-08 23:58:52 +0200

    Codeforces 1990 C

---
+++ codeforces/1990C.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define SINGLE_TEST 0
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define FOR(i, n) for (int(i) = 0; (i) < (n); ++(i))
+#define REP(i, k, n) for (int(i) = (k); (i) <= (n); ++(i))
+#define REPR(i, k, n) for (int(i) = (k); (i) >= (n); --(i))
+#define FORR(x, arr) for (auto &x : arr)
+#define FORR2(x, y, arr) for (auto &[x, y] : arr)
+#define ALL(a) (a.begin()), (a.end())
+#define RALL(a) (a.rbegin()), (a.rend())
+#define REVERSE(v) reverse(ALL(v))
+#define fi first
+#define se second
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LLINF = 1e18;
+
+void solve() {
+    int n, ans = 0; cin >> n;
+    vi a(n); 
+    FORR(x, a) cin >> x;
+
+    map<int, int> cnt;
+    set<int> s;
+
+    int z = 0;
+    while (z < n) {
+        z = 0;
+        FOR(i, n) {
+            ans += a[i];
+            if (a[i] != 0) cnt[a[i]]++;
+
+            if (cnt[a[i]] == 2) s.insert(a[i]);
+            else if (cnt[a[i]] == 1) s.erase(a[i]);
+
+            if (s.empty()) { a[i] = 0; z++; }
+            else a[i] = *s.rbegin();
+        }
+        s.clear();
+        cnt.clear();
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+#if SINGLE_TEST
+    solve();
+#else
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+#endif
+
+    return 0;
+}
