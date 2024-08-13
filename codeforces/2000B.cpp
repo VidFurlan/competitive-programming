517e2216420506d06b6c1b057ebb9dd47dc1ef1e
Vid Furlan <nalrufdiv@gmail.com>
2024-08-13 18:55:57 +0200

    Codeforces 2000 A B C D E

---
+++ codeforces/2000B.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define SINGLE_TEST 0
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define FOR(i, n) for (int(i) = 0; (i) < (n); ++(i))
+#define REP(i, k, n) for (int(i) = (k); (i) <= (n); ++(i))
+#define REPR(i, k, n) for (int(i) = (k); (i) >= (n); --(i))
+#define FORR(x, arr) for (auto &x : arr)
+#define FORR2(x, y, arr) for (auto &[x, y] : arr)
+#define ALL(a) (a.begin()), (a.end())
+#define RALL(a) (a.rbegin()), (a.rend())
+#define REVERSE(v) reverse(ALL(v))
+#define fi first
+#define se second
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LLINF = 1e18;
+
+void solve() {
+    int n; cin >> n;
+    vi a(n);
+    FORR(x, a) { cin >> x; x--; }
+    vector<bool> b(n);
+    b[a[0]] = 1;
+    REP(i, 1, n - 1) {
+        int m = 0;
+        if (a[i] > 0 && b[a[i] - 1] == 1) m++;
+        if (a[i] < n - 1 && b[a[i] + 1] == 1) m++;
+        b[a[i]] = 1;
+        if (m == 0) {
+            cout << "NO\n";
+            return;
+        }
+    }
+    cout << "YES\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+#if SINGLE_TEST
+    solve();
+#else
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+#endif
+
+    return 0;
+}
