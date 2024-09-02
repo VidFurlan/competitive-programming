eb4f4fd0a85b8362c93ec7b11199981e9ec1b768
Vid Furlan <nalrufdiv@gmail.com>
2024-09-02 21:15:22 +0200

    Codeforces 1985 F

---
+++ codeforces/1985/F.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+#define SINGLE_TEST 0
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define FOR(i, n) for (int(i) = 0; (i) < (n); ++(i))
+#define REP(i, k, n) for (int(i) = (k); (i) <= (n); ++(i))
+#define REPR(i, k, n) for (int(i) = (k); (i) >= (n); --(i))
+#define FORR(x, arr) for (auto &x : arr)
+#define FORR2(x, y, arr) for (auto &[x, y] : arr)
+#define ALL(a) (a.begin()), (a.end())
+#define RALL(a) (a.rbegin()), (a.rend())
+#define REVERSE(v) reverse(ALL(v))
+#define fi first
+#define se second
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LLINF = 1e18;
+
+void solve() {
+    ll h, n; cin >> h >> n;
+    vector<pll> a(n);
+    FOR(i, n) cin >> a[i].fi;
+    FOR(i, n) cin >> a[i].se;
+
+    priority_queue<pll, vector<pll>, greater<pll>> pq;
+    FOR(i, n) {
+        pq.push({0, i});
+    }
+
+    ll time = 0;
+    while (h > 0) {
+        auto [t, i] = pq.top();
+        time = t;
+        pq.pop();
+        h -= a[i].fi;
+        pq.push({t + a[i].se, i});
+    }
+    cout << time + 1 << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+#if SINGLE_TEST
+    solve();
+#else
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+#endif
+
+    return 0;
+}
