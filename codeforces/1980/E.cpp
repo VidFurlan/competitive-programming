83624f47c8f73828e8e900c7aacf24b808408063
Vid Furlan <nalrufdiv@gmail.com>
2024-10-25 12:33:44 +0200

    Codeforces 1980 E

---
+++ codeforces/1980/E.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define SINGLE_TEST 0
+
+typedef long long ll;
+typedef unsigned long long ull; 
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define FOR(i, n) for (int(i) = 0; (i) < (n); ++(i))
+#define REP(i, k, n) for (int(i) = (k); (i) <= (n); ++(i))
+#define REPR(i, k, n) for (int(i) = (k); (i) >= (n); --(i))
+#define FORR(x, arr) for (auto &x : arr)
+#define FORR2(x, y, arr) for (auto &[x, y] : arr)
+#define ALL(a) (a.begin()), (a.end())
+#define RALL(a) (a.rbegin()), (a.rend())
+#define REVERSE(v) reverse(ALL(v))
+#define SZ(x) (int)(x).size()
+#define fi first
+#define se second
+#define debug(x) cerr << #x << " = " << (x) << endl
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LLINF = 1e18;
+
+void solve() {
+    int n, m; cin >> n >> m;
+    vvi a(n, vi(m)), b(n, vi(m));
+    vvi c(m, vi(n)), d(m, vi(n));
+    FOR(i, n) FOR(j, m) {
+        cin >> a[i][j];
+        c[j][i] = a[i][j];
+    }
+    FOR(i, n) FOR(j, m) {
+        cin >> b[i][j];
+        d[j][i] = b[i][j];
+    }
+    FORR(r, a) sort(ALL(r));
+    FORR(r, b) sort(ALL(r));
+    sort(ALL(a));
+    sort(ALL(b));
+    FORR(r, c) sort(ALL(r));
+    FORR(r, d) sort(ALL(r));
+    sort(ALL(c));
+    sort(ALL(d));
+    cout << (a == b && c == d ? "YES" : "NO") << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+#if SINGLE_TEST
+    solve();
+#else
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+#endif
+
+    return 0;
+}
