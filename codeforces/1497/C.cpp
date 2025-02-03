c02abdfb326aafe1a4c2f09e4a0467f0e903a538
Vid Furlan <nalrufdiv@gmail.com>
2025-02-03 09:03:44 +0100

    Codeforces 1497 C

---
+++ codeforces/1497/C.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define SINGLE_TEST 0
+
+typedef long long ll;
+typedef unsigned long long ull; 
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define FOR(i, n) for (int(i) = 0; (i) < (n); ++(i))
+#define REP(i, k, n) for (int(i) = (k); (i) <= (n); ++(i))
+#define REPR(i, k, n) for (int(i) = (k); (i) >= (n); --(i))
+#define FORR(x, arr) for (auto &x : arr)
+#define FORR2(x, y, arr) for (auto &[x, y] : arr)
+#define ALL(a) (a.begin()), (a.end())
+#define RALL(a) (a.rbegin()), (a.rend())
+#define REVERSE(v) reverse(ALL(v))
+#define SZ(x) (int)(x).size()
+#define fi first
+#define se second
+#define debug(x) cerr << #x << " = " << (x) << endl
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LLINF = 1e18;
+
+void solve() {
+    int n, k; cin >> n >> k;
+    vi a(k, 1);
+    n -= k - 3;
+    if (n & 1) a[0] = a[1] = n/2;
+    else {
+        if (n % 4 == 0) {
+            a[0] = a[1] = n/4;
+            a[2] = n/2;
+        }
+        else if (n % 6 == 0) {
+            a[0] = a[1] = a[2] = n/3;
+        }
+        else {
+            a[0] = a[1] = (n-2)/2;
+            a[2] = 2;
+        }
+    }
+    FORR(x, a) cout << x << " "; cout << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+#if SINGLE_TEST
+    solve();
+#else
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+#endif
+
+    return 0;
+}
